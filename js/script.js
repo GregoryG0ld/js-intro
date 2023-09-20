2ecb764fb3388c188977b077870bce7113ae7b86
Gregory <gregory@web-zeus.com>
2023-09-20 10:31:27 +0300

    Primary Operations with Digits
    nodemon install

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-use strict;
+
+const num1 = 5;
+const num2 = 7;
+
+const result = num1 + num2;
+console.log(result);
+
+const diff = num1 - num2;
+console.log(diff);
+
+const exp1 = 2 + 2;
+const exp2 = 2 * 2;
+const exp3 = 2 ** 2;
+
+// ** - Laipsnis
+
+console.log(exp1, exp2, exp3);
